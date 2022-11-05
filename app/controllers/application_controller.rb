45449eaf5dbb3b2aad397cdeb2270f7a71b56cf3
Josekariz <joseph.macharia@student.moringaschool.com>
2022-11-05 14:49:13 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
-    { message: "Hello world" }.to_json
+    { message: "Hello from the ohter side" }.to_json
+  end
+  
+  get '/games' do
+    games = Game.all
+    games.to_json
   end
 
+  # get '/user/:id' do
+  #   games = User.find(params[:id])
+  #   games.to_json(include: :reviews)
+  #   end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(include: { reviews: { include: :user } })
+
+    end
 end
